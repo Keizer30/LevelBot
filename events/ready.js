4b9e411d42ac8f33baa382f6652aa0f3b18e9581
Keizer30 <109178236+Keizer30@users.noreply.github.com>
2022-07-13 16:14:42 +0200

    dd

--- events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = async(client, data) => {
-    console.log("bot lancé")
-    client.user.setActivity("in dev...")
-    client.channels.cache.get(process.env.STATUS_CHANNEL_ID).send("🟢 bot en ligne avec succès")
-}
